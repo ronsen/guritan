ca20353f25f682789b2f683326123733b20d4dd1
Ronald Nababan <ronsen@startrek.or.id>
2023-12-22 12:27:50 +0700

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import type { PageServerData } from "./$types";
 	import Fa from "svelte-fa";
 	import { faPencil } from "@fortawesome/free-solid-svg-icons";
+    import Alert from "$lib/components/alert.svelte";
 
 	export let data: PageServerData;
 </script>
@@ -11,22 +12,26 @@
 </svelte:head>
 
 {#if data.posts}
-	<div class="overflow-x-auto">
-		<table class="table">
-			<thead>
-				<tr class="uppercase">
-					<th>Title</th>
-					<th class="w-10">&nbsp;</th>
-				</tr>
-			</thead>
-			<tbody>
-				{#each data.posts?.items as post}
-					<tr>
-						<td><a href="/posts/{post.id}">{post.title}</a></td>
-						<td><a href="/posts/{post.id}/edit"><Fa icon={faPencil} /></a></td>
+	{#if !data.posts?.items}
+		<Alert>No posts found.</Alert>
+	{:else}
+		<div class="overflow-x-auto">
+			<table class="table">
+				<thead>
+					<tr class="uppercase">
+						<th>Title</th>
+						<th class="w-10">&nbsp;</th>
 					</tr>
-				{/each}
-			</tbody>
-		</table>
-	</div>
+				</thead>
+				<tbody>
+					{#each data.posts?.items as post}
+						<tr>
+							<td><a href="/posts/{post.id}">{post.title}</a></td>
+							<td><a href="/posts/{post.id}/edit"><Fa icon={faPencil} /></a></td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		</div>
+	{/if}
 {/if}
