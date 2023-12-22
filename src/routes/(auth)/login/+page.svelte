d0b5548898c1b747d29e60d39575a6df2680afec
Ronald Nababan <ronsen@startrek.or.id>
2023-12-22 12:11:20 +0700

    added login page

---
+++ src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Log In</title>
+</svelte:head>
+
+<div class="flex justify-center">
+	<a href="/oauth"><img src="/google-signin.png" alt="Sign in with Google" class="w-64"></a>
+</div>
