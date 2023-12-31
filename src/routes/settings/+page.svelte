603fa8dfd450a1258aa60ce880d4b76eb9de57ac
Ronald Nababan <ronsen@startrek.or.id>
2023-12-31 12:59:03 +0700

    update

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -17,7 +17,7 @@
 
 <form method="post" use:enhance>
 	<div class="mb-3">
-		<input type="number" name="blog" value="{data.blogId}" placeholder="Blog ID" class="input input-bordered w-full">
+		<input type="text" name="blog" value="{data.blogId}" placeholder="Blog ID" class="input input-bordered w-full">
 		<div class="label">
 			<div class="label-text-alt">Blog ID</div>
 		</div>
